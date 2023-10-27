6d74612047b1ed13857a0de6c96aa6f2121cc8a1
Facundo Quiroga <facu.quirogaga@gmail.com>
2023-10-27 12:57:21 -0500

    Navbar created

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import './Logo.css'
+import logo from '../../images/logo.png'
+
+const Logo = ()=>{
+  return (
+    <>
+      <img className='logo-jf' src={logo} alt="Logo de Justice Fireworks" />
+    </>
+  )
+}
+
+export default Logo
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import './Navbar.css'
+import Logo from '../Logo/Logo'
+import { useState } from 'react'
+import {useTranslation} from 'react-i18next'
+
+const Navbar = ()=>{
+  const [menu, setMenu]= useState(false)
+  const [classSpan, setClassSpan] = useState(false)
+  const openMenu = ()=>{
+    setMenu(!menu)
+    setClassSpan(!classSpan)
+  }
+
+  const [t, i18n]= useTranslation("global")
+
+  const changeLng=(lng)=>{
+    i18n.changeLanguage(lng)
+    localStorage.setItem("lng",lng)
+  }
+  return(
+    <header className="header">
+      <Logo />
+        <button className='header-button' onClick={openMenu}>
+          <span className={`top-line-${classSpan ? "clicked": "unclicked"}`}></span>
+          <span className={`middle-line-${classSpan ? "clicked": "unclicked"}`}></span>
+          <span className={`bottom-line-${classSpan ? "clicked": "unclicked"}`}></span>
+        </button>
+        <nav className={`header-nav ${menu ? "active": ""}`}>
+          <ul className="header-ul">
+            <li className="header-li" onClick={openMenu}><a href='#' className='header-a'>{t("navbar.home")}</a></li>
+            <li className="header-li" onClick={openMenu}><a href='#' className='header-a'>{t("navbar.fireworks")}</a></li>
+            <li className="header-li" onClick={openMenu}><a href='#' className='header-a'>{t("navbar.gender-reveal")}</a></li>
+            <li className="header-li" onClick={openMenu}><a href='#' className='header-a'>{t("navbar.wedding-sparklers")}</a></li>
+            <li className="header-li" onClick={openMenu}><a href='#' className='header-a'>{t("navbar.contact")}</a></li>
+            <ul className='flags'>
+              <li className="flag-li" onClick={()=>changeLng("es")}><img src="" alt="es" className='flag' /></li>
+              <li className="flag-li" onClick={()=>changeLng("en")}><img src="" alt="en" className='flag' /></li>
+            </ul>
+          </ul>
+        </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
