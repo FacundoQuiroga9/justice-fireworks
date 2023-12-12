40de08e3c0b4a74868b87207f3f91e46de3b663b
Facundo Quiroga <facu.quirogaga@gmail.com>
2023-12-12 12:05:13 -0500

    Agregada imagen de big foot

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,28 +1,25 @@
 import './About.css'
 import steve from '../../images/steve.jpg'
+import bigFoot from '../../images/big foot.png'
+import {useTranslation} from 'react-i18next'
 
 const About = () =>{
+  const [t, i18n]= useTranslation("global")
   return(
     <>
     <div id='about' className="about-container">
       <div className="about-info">
-        <h3 className='about-title'>Dynamite Fireworks</h3>
+        <h3 className='about-title'>{t("about.title1")} <span>{t("about.title2")}</span></h3>
         <p className='about-text'>
-          ¡Bienvenidos a Dynamite Fireworks! Con una selección espectacular de más de 2,000 tipos de fuegos artificiales, ofrecemos la mejor experiencia para transformar cualquier ocasión en un evento inolvidable.<br></br><br></br>
+        {t("about.text1")}<br></br><br></br>
+        {t("about.text2")}<br></br><br></br>
+        {t("about.text3")}
 
-          Nuestro propietario, Steve, es el capitán del barco con más de 20 años de experiencia en la industria. Su dedicación y conocimiento han convertido a Dynamite Fireworks en el referente indiscutible para aquellos que buscan llevar la celebración al siguiente nivel.<br></br><br></br>
-
-          En Dynamite Fireworks, nuestro equipo está orgulloso de ofrecer un servicio excepcional a nuestros clientes. Estamos comprometidos en ayudarte a encontrar la combinación perfecta de colores, formas y efectos para hacer de tu evento algo verdaderamente extraordinario.<br></br><br></br>
         </p>
       </div>
       <div className="steve-container">
-      <p className='frase'>
-        "En Dynamite Fireworks creamos momentos mágicos y experiencias únicas iluminando el cielo"
-      </p>
+      <img src={bigFoot} alt="" className="frase" />
       </div>
-
-    <div className='frase'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus saepe a quas tempore qui minus perferendis provident libero nulla. Aperiam qui non, provident ipsam atque necessitatibus? Quae quaerat in facilis laudantium, hic nobis, aut corrupti magnam doloremque atque nisi.
-    </div>
     </div>
     </>
   )
