09afcfabca42ad280ea62cf4986a024edd50b74a
Facundo Quiroga <facu.quirogaga@gmail.com>
2023-12-18 09:53:45 -0600

    Corregidas las dimensiones de las fotos en el about

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,39 @@
 import './Hero.css'
+import Video from '../../video/Dynamite2.mp4';
 import Image1 from '../../images/Dynamite.jpeg';
-import { motion } from "framer-motion"
+import Image2 from '../../images/Dynamite.jpg';
+import Image3 from '../../images/Dynamite2.jpg';
 
 const Hero = ()=> {
   return (
-    <motion.img loading='lazy' src={Image1} className="box image-custom" alt="..."
-    initial= {{ opacity:0}}
-    animate={{ opacity:1}}
-    transition={{duration:2}}
+    <>
+      <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+  <div className="carousel-item active" data-bs-interval="10000">
+      <video src={Video} autoPlay muted loop className="d-block w-100 image-custom" alt="..."/>
+    </div>
+    <div className="carousel-item " data-bs-interval="10000">
+      <img src={Image1} className="d-block w-100 image-custom" alt="..."/>
+    </div>
+    <div className="carousel-item" data-bs-interval="10000">
+      <img src={Image2} className="d-block w-100 image-custom" alt="..."/>
+    </div>
+    <div className="carousel-item" data-bs-interval="10000">
+      <img src={Image3} className="d-block w-100 image-custom" alt="..."/>
+    </div>
 
-    />
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+    </>
 
   );
 }
